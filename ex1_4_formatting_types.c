3611dfa08678d4379406aaf727160455b20b288f
seseliin <selinleyla15@gmail.com>
2026-03-15 16:19:22 +0300

    Create ex1_4_formatting_types.c

---
+++ ex1_4_formatting_types.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {   
+    printf("%c %c %c %c \n", '#', '#', '#', '#');
+    printf("%d %d %d \n", 2, 2, 2);
+    printf("%c %c \n", 't', 't');
+    printf("%d \n", 5);
+    
+    return(0);
+}
