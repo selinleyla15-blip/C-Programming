79aeb096cb2e74f01879d01ca52444c98d84bb0c
seseliin <selinleyla15@gmail.com>
2026-03-15 16:18:59 +0300

    Create ex1_3_variables.c

---
+++ ex1_3_variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("I have ");
+    printf("one month of\n");
+    printf("programming experience.\n");
+    
+    // %d icine disaridan tam sayi (integer) degerleri atama
+    printf("m=%d\nn=%d\n", 5, 7);
+    
+    return(0);
+}
